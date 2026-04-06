cf1bcdbe55fea87bff1a52746a6de9554eddecb0
Otis <milobirk@gmail.com>
2026-04-06 01:37:52 -0400

    Verify Montana Rae Evenlongername (ID: 19) - PR 3:49 from Phoenix, AZ

--- leaderboard-data.js
+++ leaderboard-data.js
@@ -1,42 +1,270 @@
 const athletes = [
     {
-        "id": 2,
-        "name": "Milo Birk",
-        "category": "male",
-        "background": "N/A",
-        "location": "Unknown",
-        "currentPR": "4.26",
-        "prCount": 2,
-        "lastAttempt": "1970-01-01",
-        "video": "#",
-        "verified": true,
+        "name": "Frank S",
+        "category": "Open Men",
+        "location": "Detriot, MI / USA / DETRIOT, MI / USA",
+        "occupation": "Parent",
+        "bodyweight": 160,
+        "dob": "2000-03-06",
+        "country": "🇺🇸",
+        "currentPR": "6:02",
+        "lastAttempt": "2026-03-03",
         "history": [
             {
-                "date": "1970-01-01",
+                "date": "2026-03-03",
+                "time": "6:02"
+            }
+        ],
+        "isVerified": false,
+        "video": "https://www.google.com/search?q=apple+tv+shows&sca_esv=04b991333a83f57d&biw=3440&bih=1317&sxsrf=ANbL-n6ZlOQC8LWXniSttjtOKwb90iyI-w%3A1773029359104&ei=70euabKKBsucptQP2OWzsQk&oq=apple&gs_lp=Egxnd3Mtd2l6LXNlcnAiBWFwcGxlKgIIATIWEC4YgAQYsQMY0QMYQxiDARjHARiKBTIOEAAYgAQYkQIYsQMYigUyChAAGIAEGEMYigUyChAAGIAEGEMYigUyChAAGIAEGEMYigUyChAAGIAEGEMYigUyChAAGIAEGEMYigUyExAuGIAEGLEDGNEDGEMYxwEYigUyFBAuGIAEGLEDGNEDGIMBGMcBGIoFSLY8UKMPWNwVcAR4AJABAJgBlgGgAc4FqgEDMS41uAEDyAEA-AEBmAILoAK9RcICChAAGLADGNYEGEfCAgQIxgnwgIKECMYgAQYJxiKBcICDRAjGIAEGCcYyQIYigXCAgsQLhiABBiRAhiKBcICERAuGIAEGLEDGNEDGIMBGMcBwgILEAAYgAQYsQMYgwHCAg4QLhiABBixAxjRAxjHAcICCxAAGIAEGJECGIoFwgIlEC4YgAQYsQMY0QMYQxiDARjHARiKBRiXBRjcBBjeBBjgBNgBAZgDAIgGAZAGCLoGBggBEAEYFJIHBzQuNS45LTKgB9ZPsgcDMC41uAeKBcIHBTAuNy40yAcigAgA&sclient=gws-wiz-serp",
+        "prCount": 1
+    },
+    {
+        "name": "Milo Birk",
+        "category": "Open Men",
+        "location": "Detroit, MI / USA / DETROIT, MI / USA",
+        "occupation": "Athlete",
+        "bodyweight": 160,
+        "dob": "1990-10-29",
+        "country": "🇺🇸",
+        "currentPR": "4:10",
+        "lastAttempt": "2026-03-07 6:16:38",
+        "history": [
+            {
+                "date": "2026-03-07 6:16:38",
                 "time": "4:09"
             },
             {
-                "date": "1970-01-01",
-                "time": "4.26"
+                "date": "2026-03-09",
+                "time": "4:10"
             }
-        ]
+        ],
+        "isVerified": true,
+        "video": "https://www.reddit.com/r/Advice/comments/1huk5e3/what_sources_you_guys_watch_for_movies_can_u_guys/",
+        "prCount": 2
     },
     {
-        "id": 3,
-        "name": "Otis B",
-        "category": "male",
-        "background": "N/A",
-        "location": "Unknown",
-        "currentPR": "0.42",
-        "prCount": 1,
-        "lastAttempt": "1970-01-01",
-        "video": "#",
-        "verified": false,
+        "name": "Random R",
+        "category": "Open Men",
+        "location": "Detroit, MI / USA / DETROIT, MI / USA",
+        "occupation": "Contractor",
+        "bodyweight": 200,
+        "dob": "1990-10-29",
+        "country": "🇺🇸",
+        "currentPR": "3:42",
+        "lastAttempt": "2026-03-08 3:07:47",
         "history": [
             {
-                "date": "1970-01-01",
-                "time": "0.42"
+                "date": "2026-03-08 3:07:47",
+                "time": "3:42"
             }
-        ]
+        ],
+        "isVerified": false,
+        "video": "https://aistudio.google.com/api-keys?project=gen-lang-client-0688610786",
+        "prCount": 1
+    },
+    {
+        "name": "Leonard Stevens",
+        "category": "Masters Male",
+        "location": "Detroit, MI / USA",
+        "occupation": "Doctor",
+        "bodyweight": 140,
+        "dob": "1972-03-10",
+        "country": "🇺🇸",
+        "currentPR": "3:12",
+        "lastAttempt": "2026-03-13",
+        "history": [
+            {
+                "date": "2026-03-13",
+                "time": "3:12"
+            }
+        ],
+        "isVerified": false,
+        "video": "https://app.spectora.com/inspection/544427e3-e565-4ac4-9e65-4b51f8738350/editf",
+        "prCount": 1
+    },
+    {
+        "name": "Bryant Garcia",
+        "category": "Open Men",
+        "location": "Detroit, MI / USA",
+        "occupation": "Athlete",
+        "bodyweight": 150,
+        "dob": "1990-10-29",
+        "country": "🇺🇸",
+        "currentPR": "3:01",
+        "lastAttempt": "2026-03-10",
+        "history": [
+            {
+                "date": "2026-03-10",
+                "time": "3:01"
+            }
+        ],
+        "isVerified": true,
+        "video": "https://tally.so/forms/RGzOe4/edit",
+        "prCount": 1
+    },
+    {
+        "name": "Testa Longername",
+        "category": "Open Men",
+        "location": "Detroit, MI / USA / DETROIT, MI / USA",
+        "occupation": "Test Proffessional",
+        "bodyweight": 120,
+        "dob": "1990-10-29",
+        "country": "🇺🇸",
+        "currentPR": "2:35",
+        "lastAttempt": "2026-03-08 6:31:02",
+        "history": [
+            {
+                "date": "2026-03-08 6:31:02",
+                "time": "2:20"
+            },
+            {
+                "date": "2026-03-09",
+                "time": "2:35"
+            }
+        ],
+        "isVerified": false,
+        "video": "https://www.youtube.com/",
+        "prCount": 2
+    },
+    {
+        "name": "Megan Mullins",
+        "category": "Open Women",
+        "location": "Mount Pleasant, MI / USA",
+        "occupation": "Bank teller",
+        "bodyweight": 125,
+        "dob": "1998-12-09",
+        "country": "🇺🇸",
+        "currentPR": "1:30",
+        "lastAttempt": "2026-03-14",
+        "history": [
+            {
+                "date": "2026-03-14",
+                "time": "1:30"
+            }
+        ],
+        "isVerified": false,
+        "video": "https://drive.google.com/file/d/13WbYyva3Yl44kUeMZNiv7Y6fJHOvR98E/view?usp=drivesdk",
+        "prCount": 1
+    },
+    {
+        "name": "Zak Mullins",
+        "category": "Open Men",
+        "location": "Mount Pleasant, MI / USA",
+        "occupation": "Retail",
+        "bodyweight": 195,
+        "dob": "1996-05-10",
+        "country": "🇺🇸",
+        "currentPR": "1:20",
+        "lastAttempt": "2026-03-13",
+        "history": [
+            {
+                "date": "2026-03-13",
+                "time": "1:20"
+            }
+        ],
+        "isVerified": false,
+        "video": "https://drive.google.com/file/d/1r3eeMYAcN6amJ2ZNBEvvMHlNqbNXYbo6/view?usp=drivesdk",
+        "prCount": 1
+    },
+    {
+        "name": "Markie Mullins",
+        "category": "Open Women",
+        "location": "Detroit, MI / USA / DETROIT, MI / USA",
+        "occupation": "Athlete",
+        "bodyweight": 170,
+        "dob": "1992-07-17",
+        "country": "🇺🇸",
+        "currentPR": "1:16",
+        "lastAttempt": "2026-03-07 4:46:31",
+        "history": [
+            {
+                "date": "2026-03-07 4:46:31",
+                "time": "1:16"
+            }
+        ],
+        "isVerified": false,
+        "video": "https://webhook.site/#!/view/efa2a342-0c51-4b94-a9f1-e9ea586f996a/d44730c2-bf62-40b9-97a3-e9ff278f561e/1",
+        "prCount": 1
+    },
+    {
+        "name": "Venus M",
+        "category": "Open Women",
+        "location": "Detroit, MI / USA / DETROIT, MI / USA",
+        "occupation": "Astrologer",
+        "bodyweight": 170,
+        "dob": "1990-10-29",
+        "country": "🇺🇸",
+        "currentPR": "1:08",
+        "lastAttempt": "2026-03-07 7:11:58",
+        "history": [
+            {
+                "date": "2026-03-07 7:11:58",
+                "time": "1:08"
+            }
+        ],
+        "isVerified": false,
+        "video": "https://support.spectora.com/en/articles/2013085-what-s-included-in-a-home-inspection-infographic",
+        "prCount": 1
+    },
+    {
+        "name": "Joshua Taylor",
+        "category": "Masters Female",
+        "location": "Detroit, MI / USA",
+        "occupation": "Community Outreach",
+        "bodyweight": 180,
+        "dob": "1967-10-12",
+        "country": "🇺🇸",
+        "currentPR": "0:50",
+        "lastAttempt": "2026-03-11",
+        "history": [
+            {
+                "date": "2026-03-11",
+                "time": "0:50"
+            }
+        ],
+        "isVerified": false,
+        "video": "https://www.mulerouter.ai/app/api-keys",
+        "prCount": 1
+    },
+    {
+        "name": "Otis B",
+        "category": "Open Men",
+        "location": "Detroit, MI / USA / DETROIT, MI / USA",
+        "occupation": "Athlete",
+        "bodyweight": 180,
+        "dob": "1990-10-29",
+        "country": "🇺🇸",
+        "currentPR": "0:42",
+        "lastAttempt": "2026-03-07 6:29:51",
+        "history": [
+            {
+                "date": "2026-03-07 6:29:51",
+                "time": "0:42"
+            }
+        ],
+        "isVerified": false,
+        "video": "https://support.spectora.com/en/articles/2013085-what-s-included-in-a-home-inspection-infographic",
+        "prCount": 1
+    },
+    {
+        "name": "Pepper Chonie",
+        "category": "Open Men",
+        "location": "Detroit, MI / USA / DETROIT, MI / USA",
+        "occupation": "Security",
+        "bodyweight": 150,
+        "dob": "1990-10-29",
+        "country": "🇺🇸",
+        "currentPR": "0:30",
+        "lastAttempt": "2026-03-08 18:40:02",
+        "history": [
+            {
+                "date": "2026-03-08 18:40:02",
+                "time": "0:30"
+            }
+        ],
+        "isVerified": false,
+        "video": "https://www.youtube.com/",
+        "prCount": 1
     }
 ];
